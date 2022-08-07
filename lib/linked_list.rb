0eb8cd7cea8fcca235ac54babf3307905f150108
Nikhil Shahi <shniks@gmail.com>
2022-08-07 11:12:09 -0700

    Created count method in linkedlist class.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,14 +3,16 @@ require 'pry'
 
 class LinkedList
 
-    attr_reader :head
+    attr_reader :head, :count
 
   def initialize
     @head = nil
+    @count = 0
   end
 
   def append(surname)
     @head = Node.new(surname)
+    @count += 1
   end
 
 end
