61f8f418a5dbd7bf67fee0caa6ee5902b5564509
Nikhil Shahi <shniks@gmail.com>
2022-08-07 12:46:40 -0700

    Refactored append method into smaller methods.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -14,17 +14,29 @@ class LinkedList
     if head.nil?
       @head = Node.new(surname)
     else
-      current_node = @head
-      while current_node.next_node != nil
-        current_node = current_node.next_node
-      end
-      current_node.next_node = Node.new(surname)
+      current_node = head
+      append_traverse(current_node, surname)
     end
     @count += 1
   end
 
+  def append_traverse(current_node, surname)
+    while current_node.next_node != nil
+      current_node = current_node.next_node
+    end
+    insert_node_after_current_node(current_node, surname)
+  end
+
+  def insert_node_after_current_node(current_node, surname)
+    current_node.next_node = Node.new(surname)
+  end
+
   def to_string
-    "The #{@head.surname} family"
+    if @head.next_node.nil?
+      @s = "The #{@head.surname} family"
+    else
+      @s += ", followed by the #{@current_node.next_node.surname} family"
+    end
   end
 
 end
