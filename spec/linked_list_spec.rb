4db1fbba45ba2d4203ec482fbf343b4c936b7e3d
Nikhil Shahi <shniks@gmail.com>
2022-08-07 08:59:04 -0700

    Created test to see if it can append a node.

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -10,13 +10,18 @@ RSpec.describe LinkedList do
   end
 
   it 'is an instance of an object' do
-
     expect(@list).to be_a LinkedList
   end
 
   it 'has attributes' do
-
     expect(@list.head).to eq(nil)
+  end
+
+  it 'can append a node' do
+    @list.append("West")
+
+    expect(@list.head.surname).to eq("West")
+    expect(@list.head.next_node).to eq(nil)
   end
 
 end
