5523b00089786fb4fdd2e622eea9db0f0f9b3f54
Nikhil Shahi <shniks@gmail.com>
2022-08-07 11:33:49 -0700

    Created a test to see if it can append, to_string and count for multiple nodes)

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -38,4 +38,19 @@ RSpec.describe LinkedList do
     expect(@list.to_string).to eq("The West family")
   end
 
+  it 'can append, to_string and count for multiple nodes' do
+    @list.append("Rhodes")
+
+    expect(@list.head.surname).to eq("Rhodes")
+    expect(@list.head.next_node).to eq(nil)
+
+    @list.append("Hardy")
+
+    expect(@list.head.next_node).to be_a Node
+    expect(@list.head.next_node.surname).to eq("Hardy")
+    expect(@list.head.next_node.next_node).to eq(nil)
+    expect(@list.count).to eq(2)
+    expect(@list.to_string).to eq("The Rhodes family, followed by the Hardy family")
+  end
+
 end
