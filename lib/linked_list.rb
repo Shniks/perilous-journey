505cf330b44e8ca75888bed1c5755f0d83ebf812
Nikhil Shahi <shniks@gmail.com>
2022-08-07 12:58:28 -0700

    refactored traverse method in linkedlist class.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -12,19 +12,31 @@ class LinkedList
 
   def append(surname)
     if head.nil?
-      @head = Node.new(surname)
+      append_node_if_head_nil(surname)
     else
-      current_node = head
-      append_traverse(current_node, surname)
+      append_node_if_head_not_nil(surname)
     end
     @count += 1
   end
 
+  def append_node_if_head_nil(surname)
+    @head = Node.new(surname)
+  end
+
+  def append_node_if_head_not_nil(surname)
+    current_node = head
+    append_traverse(current_node, surname)
+  end
+
   def append_traverse(current_node, surname)
+    append_traverse_if_next_node_not_nil(current_node)
+    insert_node_after_current_node(current_node, surname)
+  end
+
+  def append_traverse_if_next_node_not_nil(current_node)
     while current_node.next_node != nil
       current_node = current_node.next_node
     end
-    insert_node_after_current_node(current_node, surname)
   end
 
   def insert_node_after_current_node(current_node, surname)
@@ -33,7 +45,7 @@ class LinkedList
 
   def to_string
     if @head.next_node.nil?
-      @s = "The #{@head.surname} family"
+      @s = "The #{head.surname} family"
     else
       @s += ", followed by the #{@current_node.next_node.surname} family"
     end
