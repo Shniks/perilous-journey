becba926f55f17fa5bf9efaa6ccf2afcf79d8c6d
Nikhil Shahi <shniks@gmail.com>
2022-08-09 09:59:22 -0700

    Updated pop test to output correct message to terminal upon a pop. Completed iteration 5.

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -132,7 +132,7 @@ RSpec.describe LinkedList do
     @list.insert(1, "Lawson")
 
     expect{@list.pop}.to output("The Henderson family has died of dysentery").to_stdout
-    expect(@list.pop).to be_a Node
+    expect{@list.pop}.to output("The Brooks family has died of dysentery").to_stdout
     expect(@list.to_string).to eq("The McKinney family, followed by the Lawson family")
   end
 
