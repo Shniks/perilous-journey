1fcbffed5541e40ef5caa589f1fe3b0ca51f7e5e
Nikhil Shahi <shniks@gmail.com>
2022-08-10 07:11:32 -0700

    Updated node spec by adding supplies.

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -5,15 +5,16 @@ require './lib/node'
 
 RSpec.describe Node do
   it 'should be an instance of an object' do
-    node = Node.new("Burke")
+    node = Node.new("Burke", {"pounds of food" => 200})
 
     expect(node).to be_a Node
   end
 
   it 'should be have attributes' do
-    node = Node.new("Burke")
+    node = Node.new("Burke", {"pounds of food" => 200})
 
     expect(node.surname).to eq("Burke")
+    expect(node.supplies).to eq({"pounds of food" => 200})
     expect(node.next_node).to be nil
   end
 
